d7ec0c18567380561a3af161e270dd08687b6151
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-04 19:46:09 -0300

    feat: created a example model

---
+++ WebApiDemo/Models/Shirt.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApiDemo.Models.Validation;
+
+namespace WebApiDemo.Models
+{
+    public class Shirt
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Brand { get; set; }
+
+        [Required]
+        public string? Color { get; set; }
+
+        [Shirt_EnsureCorrectSizing]
+        public int Size { get; set; }
+
+        [Required]
+        public string? Gender { get; set; }
+
+        public double Price { get; set; }
+
+    }
+}
