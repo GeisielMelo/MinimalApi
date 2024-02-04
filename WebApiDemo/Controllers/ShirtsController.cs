209d1ff5e7105a6d01dfb27a5215ff861dfccd62
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-04 12:38:10 -0300

    Add project files.

---
+++ WebApiDemo/Controllers/ShirtsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsController: ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Index: Indexed all the shirts.";
+        }
+
+        [HttpGet("{id}")]
+        public string Show(int id)
+        {
+            return $"Show: {id}";
+        }
+
+        [HttpPost]
+        public string Create()
+        {
+            return "Create: Created.";
+        }
+
+        [HttpPut("{id}")]
+        public string Update(int id)
+        {
+            return $"Update: {id}.";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Delete: {id}.";
+        }
+    }
+}
