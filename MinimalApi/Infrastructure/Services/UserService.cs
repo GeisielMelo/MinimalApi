de34c667d70e7604b88187f9ea3c036eed0670a6
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-07 23:07:27 -0300

    feat: repository renamed to service for consistence

--- MinimalApi/Infrastructure/Services/UserService.cs
+++ MinimalApi/Infrastructure/Services/UserService.cs
@@ -3,17 +3,16 @@ using MongoDB.Driver;
 using MongoDB.Bson;
 using MinimalApi.Domain.Models;
 
-namespace MinimalApi.Infrastructure.Repositories
+namespace MinimalApi.Infrastructure.Services
 {
-    public class UserRepository
+    public class UserService
     {
         private readonly IMongoCollection<User> _userCollection;
 
-        public UserRepository(IOptions<MongoDBSettings> mongoDBSettings) {
+        public UserService(IOptions<MongoDBSettings> mongoDBSettings) {
             MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
             IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
             _userCollection = database.GetCollection<User>("users");
-            
         }
 
         public async Task CreateUser(User user) {
@@ -27,6 +26,10 @@ namespace MinimalApi.Infrastructure.Repositories
 
         public async Task<User> GetUserById(string id) {
             return await _userCollection.Find(user => user.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<User> GetByEmail(string email) {
+            return await _userCollection.Find(user => user.Email == email).FirstOrDefaultAsync();
         }
 
         public async Task UpdateUser(User user) {
