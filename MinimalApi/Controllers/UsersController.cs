07e99bd5da0e104663eee8d19d23dedbf5b72b41
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-07 23:09:30 -0300

    feat: add [Authorize]; update UserRepository

--- MinimalApi/Controllers/UsersController.cs
+++ MinimalApi/Controllers/UsersController.cs
@@ -1,8 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using MinimalApi.Infrastructure.Repositories;
 using MinimalApi.Domain.Models;
-
 using MinimalApi.Filters;
+using MinimalApi.Infrastructure.Services;
 
 namespace MinimalApi.Controllers
 {
@@ -11,17 +11,19 @@ namespace MinimalApi.Controllers
     public class UsersController : ControllerBase
     {
 
-        private readonly UserRepository _mongoDBService;
+        private readonly UserService _mongoDBService;
 
-        public UsersController(UserRepository mongoDBService) {
+        public UsersController(UserService mongoDBService) {
             _mongoDBService = mongoDBService;
         }
 
+        [Authorize]
         [HttpGet]
         public async Task<IActionResult> Index() {
             return Ok(await _mongoDBService.GetAllUsers());
         }
 
+        [Authorize]
         [HttpGet("{id}")]
         [User_ValidateUserIdFilter]
         public async Task<IActionResult> Show(string id) {
@@ -34,12 +36,14 @@ namespace MinimalApi.Controllers
             return Ok("User created successfully.");
         }
 
+        [Authorize]
         [HttpPut]
         public async Task<IActionResult> Update([FromBody]User user) {
             await _mongoDBService.UpdateUser(user);
             return Ok("User information updated successfully.");
         }
 
+        [Authorize]
         [HttpDelete("{id}")]
         [User_ValidateUserIdFilter]
         public async Task<IActionResult> Delete(string id) {
