5d7d48b8d8d3e4ec51874a5abb1957befa309570
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-07 23:06:43 -0300

    feat: add jwt

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,16 +1,42 @@
 using MinimalApi.Infrastructure;
-using MinimalApi.Infrastructure.Repositories;
+using MinimalApi.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDB"));
-builder.Services.AddSingleton<UserRepository>();
+builder.Services.AddSingleton<UserService>();
+builder.Services.AddSingleton<AuthService>();
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication( x => {
+  x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+  x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+  x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => {
+  x.TokenValidationParameters = new TokenValidationParameters {
+    ValidIssuer = "admin",
+    ValidAudience = "admin",
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("admin")),
+    ValidateIssuer = true,
+    ValidateAudience = true,
+    ValidateLifetime = true,
+    ValidateIssuerSigningKey = true,
+  };
+});
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
+
+// https://youtu.be/mgeuh8k3I4g?t=310
