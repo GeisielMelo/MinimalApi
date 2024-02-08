147b831671b014b547ec241373ab49d8481a5e60
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-07 23:10:10 -0300

    feat: context data for auth with SQL.
    will be removed

---
+++ MinimalApi/Application/Context/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MinimalApi.Application.Context
+{
+    public class DataContext : IdentityDbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
+
